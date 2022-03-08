6cc78b0bf1bd6566728060dd3919b1f93b2f0af0
leandro lucero <leandrodalmirolu@gmail.com>
2022-03-08 20:00:20 -0300

    agregue js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let usuario1, usuario2;
+
+function acceder() {
+  alert("Por favor complete su usuario y su password");
+
+  usuario1 = prompt('Introduzca su usuario "Administrador" ');
+  usuario2 = prompt('Introduzca su password "1234" ');
+  console.log(usuario1);
+  console.log(usuario2);
+
+  alert("Hola mundo Bienvenidos");
+  usuario1 = parseFloat(usuario1);
+  usuario2 = parseFloat(usuario2);
+}
